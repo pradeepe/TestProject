a1e652e884a9fd000f6b3a9914ca990d13591df6
Pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2018-10-14 15:32:01 -0700

    Adding HelloWorldTest.java test class

---
+++ src/test/java/com/org/learningmaven/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.org.learningmaven;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	@Test
+	public void login() {
+		System.out.println("Login into Application");
+	}
+
+}
